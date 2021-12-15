65548cbd253254e4f2be8da7a8085333399fcb92
rachel <rachelcwhite1996@gmail.com>
2021-12-15 17:55:49 -0500

    mediaroutes, started shelves routes

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,12 +2,14 @@ const express = require('express')
 const usersRouter = require('./users.js');
 const sessionRouter = require('./session')
 const mediaRouter = require('./media')
+const shelfRouter = require('./shelf')
 
 const router = express.Router();
 
 router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 router.use('/media', mediaRouter)
+router.use('/shelves', shelfRouter)
 
 // // POST test route
 // router.post('/test', function (req, res) {
--- backend/routes/api/shelf.js
+++ backend/routes/api/shelf.js
@@ -4,20 +4,15 @@ const { restoreUser } = require('../../utils/auth');
 
 const router = express.Router();
 
-const { Media } = require('../../db/models');
+const { Shelf } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
 
 router.get('/', asyncHandler(async(req, res) => {
-    const media = await Media.findAll()
-    console.log(res.json(media))
-    return res.json(media)
+    const shelves = await Shelf.findAll()
+    return res.json(shelves)
 }))
 
-router.get('/:id(\\d+)', asyncHandler(async(req, res) => {
-    const media = await Media.findByPk(req.params.id)
-    return res.json(media)
-}))
 
 module.exports = router;
