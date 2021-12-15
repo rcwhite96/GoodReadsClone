34670563e3a358572dea91c132f04ebbfb0a198a
rachel <rachelcwhite1996@gmail.com>
2021-12-15 17:18:45 -0500

    routes bug

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const usersRouter = require('./users.js');
 const sessionRouter = require('./session')
+const mediaRouter = require('./media')
 
 const router = express.Router();
 
 router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
+router.use('/media', mediaRouter)
 
 // // POST test route
 // router.post('/test', function (req, res) {
---
+++ backend/routes/api/media.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const asyncHandler = require('express-async-handler');
+const { restoreUser } = require('../../utils/auth');
+
+const router = express.Router();
+
+const { Media } = require('../../db/models');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+
+router.get('/media', asyncHandler(async(req, res) => {
+    const media = await Media.findAll()
+    console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+    console.log(res.json(media))
+    return res.json(media)
+}))
+
+router.get('/media/:id(\\d+)', asyncHandler(async(req, res) => {
+    const media = await Media.findByPk(req.params.id)
+    return res.json(media)
+}))
+
+module.exports = router;
