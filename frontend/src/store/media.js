f02d0d0f732aa9362fbd65851e2a0f0ee1f59261
rachel <rachelcwhite1996@gmail.com>
2021-12-16 10:17:58 -0500

    redux store and component for all media

--- frontend/src/store/media.js
+++ frontend/src/store/media.js
@@ -16,14 +16,7 @@ export const getAll = () => async dispatch =>{
     if(res.ok){
         const data = await res.json()
         dispatch(getAllMedia(data))
-        return data
-    } else if (res.status < 500){
-        const data = await res.json()
-        if(data.errors) {
-            return data.errors
-        }
-    } else {
-        return ['An error has occured']
+
     }
 }
 
