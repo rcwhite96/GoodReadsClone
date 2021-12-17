c31ea5dc53ad0c86510081fbcd33c854be3e35c9
rachel <rachelcwhite1996@gmail.com>
2021-12-17 10:10:55 -0500

    one project

--- frontend/src/components/OneMediaPage/OneMediaPage.js
+++ frontend/src/components/OneMediaPage/OneMediaPage.js
@@ -1,22 +1,27 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from 'react-redux';
-import{ NavLink, useParams } from 'react-router-dom'
+import{ useParams } from 'react-router-dom'
 import { getOne } from  '../../store/media'
 
 export default function OneMediaPage(){
     let dispatch = useDispatch()
     let currentMedia = useSelector(state => state.media)
+    console.log(currentMedia + "AAAAAAAAAAAAAAAAAAAAAAAAA")
     const {mediaId} = useParams()
 
     useEffect(() => {
         dispatch(getOne(mediaId))
+        console.log(getOne(mediaId) + "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
     }, [dispatch])
 
     return(
         <>
         <div>{currentMedia.title}</div>
-            <img className="media-image" src={currentMedia.imageURL[0]} alt="media-img"/>
+        <img className="media-image" src={currentMedia.imageURL} alt="media-img"/>
+        <div className="title">{currentMedia?.title}</div>
+        <div className="title">{currentMedia?.creator}</div>
+        <div className="title">{currentMedia?.mediaType}</div>
+        <div className="title">{currentMedia?.description}</div>
         </>
     )
 }
-
