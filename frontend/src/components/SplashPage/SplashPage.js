3623a3daff56ff732d53d0a7de8ab127bb8e103c
rachel <rachelcwhite1996@gmail.com>
2022-02-10 13:21:30 -0500

    carousel

--- frontend/src/components/SplashPage/SplashPage.js
+++ frontend/src/components/SplashPage/SplashPage.js
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 import { useDispatch } from 'react-redux';
 import{ NavLink } from 'react-router-dom'
 import './SplashPage.css'
-import logo from './pngaaa.com-3543360.png'
 import { getShelves } from  '../../store/shelf'
 
 export default function SplashPage(){
@@ -16,12 +15,7 @@ export default function SplashPage(){
         <>
             <h1>Check Out Cyberpunk Media Today</h1>
             <p className="desc">Explore various cyberpunk media, whether it be books, games, movies or TV. Add your favorites to your shelves and let other users know if you enjoyed them!</p>
-            <div className="logo-container"><img src={logo} alt="logo" className="logo"/></div>
-            <div className="btn-div">
-                <NavLink to="/signup">
-                    <button className="media-btn">Sign Up</button>
-                </NavLink>
-            </div>
+            
         </>
     )
 }
